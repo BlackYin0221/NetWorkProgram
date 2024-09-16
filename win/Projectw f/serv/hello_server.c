d58741dd021685f3720a005cc80ed4807bd7edca
yinxunpeng <yinxunpeng377@163.com>
2024-09-16 14:43:38 +0800

    feat(tcp): htons and htonl

--- win/Projectw f/serv/hello_server.c
+++ win/Projectw f/serv/hello_server.c
@@ -2,8 +2,22 @@
 #include <stdlib.h>
 #include <WinSock2.h>
 void error_handling(char* message);
+void hello_world(int argc, char* argv[]);
+void endian_conv();
 
 int main(int argc, char* argv[])
+{
+	endian_conv();
+}
+
+void error_handling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+void hello_world(int argc, char* argv[])
 {
 	WSADATA wsaData;
 	SOCKET hServSock, hClntSock;
@@ -23,7 +37,7 @@ int main(int argc, char* argv[])
 	}
 
 	hServSock = socket(PF_INET, SOCK_STREAM, 0);
-	if (hServSock == INVALID_SOCKET){
+	if (hServSock == INVALID_SOCKET) {
 		error_handling("socket() error");
 	}
 
@@ -32,17 +46,17 @@ int main(int argc, char* argv[])
 	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servAddr.sin_port = htons(atoi(argv[1]));
 
-	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR){
+	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
 		error_handling("bind() error");
 	}
 
-	if (listen(hServSock, 5) == -1){
+	if (listen(hServSock, 5) == -1) {
 		error_handling("listen() error");
 	}
 
 	szClntAddr = sizeof(clntAddr);
 	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
-	if (hClntSock == INVALID_SOCKET){
+	if (hClntSock == INVALID_SOCKET) {
 		error_handling("accpt() error");
 	}
 
@@ -53,9 +67,25 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void error_handling(char* message)
+void endian_conv()
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+	WSADATA wsaData;
+	unsigned short host_port = 0x1234;
+	unsigned short net_port;
+	unsigned long host_addr = 0x12345678;
+	unsigned long net_addr;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		error_handling("WSAStartup() error!");
+	}
+	net_port = htons(host_port);
+	net_addr = htonl(host_addr);
+
+	printf("Host orderd port: %#x \n", host_port);
+	printf("Network orderd port: %#x \n", net_port);
+	printf("Host orderd addrs: %#lx \n", host_addr);
+	printf("Network orderd port: %#lx \n", net_addr);
+	WSACleanup();
+	return;
+
 }
